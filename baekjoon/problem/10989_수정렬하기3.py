9d194d202a48ea8a27482346a2d39d73eb3b47c8
hjihwan16 <hjihwan16@gmail.com>
2023-08-01 17:50:38 +0900

    done

--- baekjoon/problem/10989_수정렬하기3.py
+++ baekjoon/problem/10989_수정렬하기3.py
@@ -1,24 +1,19 @@
 # 수 정렬하기 3
 # 수의 범위가 적다면 카운팅 정렬을 사용하여 더욱 빠르게 정렬할 수 있습니다.
 # list.sort() 가 오래걸림
-
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-num_list = []
+cnt_list = [0] * (10001)
+
+# num을 입력받을 때 마다 cnt 하나씩 올리기.
 for i in range(N):
     num = int(input())
-    num_list.append(num)
-
-cnt_list = [0] * (max(num_list) + 1)
-
-for num in num_list:
     cnt_list[num] += 1
 
-# print(cnt_list)
+for k in range(10001):
+    cnt = cnt_list[k]
+    for j in range(cnt):
+        print(k)
 
-for k in range(len(cnt_list)):
-    if cnt_list[k] == 0:
-        pass
-    else:
-        for l in range(cnt_list[k]):
-            print(k)
